f32cba2db24b01d6dad915084e52074e80dcda24
Trung Nguyen Thanh 2 <trungnguyent5@HarveyNash.vn.local>
2017-06-20 17:21:59 +0700

    initial commit

---
+++ src/main/java/com/trungee/shop_teller/ShopTellerApplication.java
@@ -0,0 +1,12 @@
+package com.trungee.shop_teller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopTellerApplication.class, args);
+	}
+}
